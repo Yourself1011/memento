ec428e5cc0085d8884f01aa12c4cab2f6af7433c
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-05-06 08:20:51 -0400

    feat(full): MomentList component

---
+++ client/src/components/MomentList/MomentList.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import { Moment } from '../../types/moment';
+
+const MomentList = ({className=''}) => {
+
+  const moments = JSON.parse(localStorage.getItem("moments") as string);
+
+  return (
+    <div className={`pages-container ${className}`}>
+      <div>
+        <p>Name</p>
+        <p>Date Created</p>
+      </div>
+      {
+        moments.map((value:Moment, index:number) => {
+          return (
+            <div>
+              <Link to={`/edit/${index}`}>{value.name}</Link>
+              <p>{value.createdDate}</p>
+            </div>
+          )
+        })
+      }
+    </div>
+)
+}
+
+export default MomentList
--- client/src/pages/Moments.tsx
+++ client/src/pages/Moments.tsx
@@ -1,3 +1,5 @@
+import MomentList from "../components/MomentList/MomentList"
+
 const Moments = () => {
   return (
     <div className='w-full h-full'>
@@ -5,24 +7,7 @@ const Moments = () => {
         <h1>Moments</h1>
         <p className='flex items-center'>A comprehensive list of all your Moments</p>
       </div>
-      <div className='pages-container m-8'>
-        <div>
-          <p>Name</p>
-          <p>Date Created</p>
-        </div>
-        <div>
-          <a>Schindler's List Reflection</a>
-          <p>01/01/23</p>
-        </div>
-        <div>
-          <a>Acids and Bases</a>
-          <p>05/05/23</p>
-        </div>
-        <div>
-          <a>Intro to Accounting</a>
-          <p>69/69/69</p>
-        </div>
-      </div>
+      <MomentList className='m-8'/>
     </div>
   )
 }
