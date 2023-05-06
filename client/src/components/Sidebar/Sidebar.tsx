be7b37a404bfe4e2fbf13968ee717dc29e414f70
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-05-06 01:26:18 -0400

    fix(front): fix sidebar

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -5,27 +5,32 @@ import { NavLink } from "react-router-dom";
 import "./Sidebar.scss";
 
 function Sidebar() {
-    return (
-        <div className="border-r-[#e7e7ef] p-6 border-r-2 flex flex-col sidebar">
-            <p className="font-bold text-2xl text-center">Amonus</p>
-            <NavLink to="/">
-                <GrHomeRounded />
-                Home
-            </NavLink>
-            <NavLink to='/pages'>
-            <AiOutlineUnorderedList/>
-            Pages
-          </NavLink>
-          <NavLink to="/edit/0">
-                <GrDocument />
-                Edit
-            </NavLink>
-            <NavLink to="/cards">
-                <BsCardList />
-                Flashcards
-            </NavLink>
-        </div>
-    );
+  return (
+    <div className="border-r-[#e7e7ef] border-r-2 flex flex-col justify-between shrink-0 sidebar">
+      <div className='px-6'>
+        <p className="font-bold text-2xl py-6 flex items-center justify-between">Amonus <p>logo</p></p>
+        <NavLink to="/">
+          <GrHomeRounded />
+          Home
+        </NavLink>
+        <NavLink to='/pages'>
+          <AiOutlineUnorderedList/>
+          Pages
+        </NavLink>
+        <NavLink to="/edit/0">
+          <GrDocument />
+          Edit
+        </NavLink>
+        <NavLink to="/cards">
+          <BsCardList />
+          Flashcards
+        </NavLink>
+      </div>
+      <div>
+        <a className='create'>+ Create</a>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
