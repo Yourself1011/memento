6363c98ee6adb87d1796f08b9d3a953d26ca33ce
Daniel Zhang <d.zhang200788@gmail.com>
2023-05-06 12:26:04 -0400

    create mememento

--- client/src/components/Create/Create.tsx
+++ client/src/components/Create/Create.tsx
@@ -1,11 +1,39 @@
+import { useState } from "react";
+import { GrDocument } from "react-icons/gr";
+import { BsCardList } from "react-icons/bs";
+import { createMoment } from "../../utils/createMoment";
+import { useNavigate } from "react-router-dom";
 import "./Create.scss";
 
 function Create() {
-    return (
-        <div>
-            <a className="create">+ Create</a>
-        </div>
-    );
+  const [display, setDisplay] = useState(false);
+  const navigate = useNavigate();
+
+  return (
+    <div className="create">
+      <div className={`drawer ${display ? "show" : "hide"}`}>
+        <a
+          className="menuItems"
+          onClick={() => {
+            const momentId = createMoment();
+            navigate(`/edit/${momentId}`);
+            location.reload();
+            setDisplay(false);
+          }}
+        >
+          <GrDocument />
+          New Moment
+        </a>
+        <a className="menuItems">
+          <BsCardList />
+          New Flashcard
+        </a>
+      </div>
+      <a className="highlight" onClick={() => setDisplay(!display)}>
+        + Create
+      </a>
+    </div>
+  );
 }
 
 export default Create;
--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -7,48 +7,47 @@ import { useEffect, useState } from "react";
 import "./Sidebar.scss";
 
 function Sidebar() {
-    const navigate = useNavigate();
-    const [lastEdited, setLastEdited] = useState(
-        localStorage.getItem("lastEdited") ?? "0"
-    );
+  const navigate = useNavigate();
+  const [lastEdited, setLastEdited] = useState(
+    localStorage.getItem("lastEdited") ?? "0"
+  );
 
-    function storageChange() {
-        setLastEdited(localStorage.getItem("lastEdited") ?? lastEdited);
-        console.log("among");
-    }
+  function storageChange() {
+    setLastEdited(localStorage.getItem("lastEdited") ?? lastEdited);
+  }
 
-    useEffect(() => {
-        storageChange();
-        window.addEventListener("storage", storageChange);
-        return window.removeEventListener("storage", storageChange);
-    }, [navigate]);
+  useEffect(() => {
+    storageChange();
+    window.addEventListener("storage", storageChange);
+    return window.removeEventListener("storage", storageChange);
+  }, [navigate]);
 
-    return (
-        <div className="border-r-[#e7e7ef] border-r-2 flex flex-col justify-between shrink-0 sidebar">
-            <div className="px-6">
-                <p className="font-bold text-2xl py-6 flex items-center justify-between">
-                    Memento <p>logo</p>
-                </p>
-                <NavLink to="/">
-                    <GrHomeRounded />
-                    Home
-                </NavLink>
-                <NavLink to="/moments">
-                    <AiOutlineUnorderedList />
-                    Moments
-                </NavLink>
-                <NavLink to={`/edit/${lastEdited}`}>
-                    <GrDocument />
-                    Edit
-                </NavLink>
-                <NavLink to="/cards">
-                    <BsCardList />
-                    Flashcards
-                </NavLink>
-            </div>
-            <Create />
-        </div>
-    );
+  return (
+    <div className="border-r-[#e7e7ef] border-r-2 flex flex-col justify-between shrink-0 sidebar">
+      <div className="px-6">
+        <p className="font-bold text-2xl py-6 flex items-center justify-between">
+          Memento <p>logo</p>
+        </p>
+        <NavLink to="/">
+          <GrHomeRounded />
+          Home
+        </NavLink>
+        <NavLink to="/moments">
+          <AiOutlineUnorderedList />
+          Moments
+        </NavLink>
+        <NavLink to={`/edit/${lastEdited}`}>
+          <GrDocument />
+          Edit
+        </NavLink>
+        <NavLink to="/cards">
+          <BsCardList />
+          Flashcards
+        </NavLink>
+      </div>
+      <Create />
+    </div>
+  );
 }
 
 export default Sidebar;
