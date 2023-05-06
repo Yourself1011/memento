ae10bb092bf6a6e77ae9299cac7e205b3531fed3
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-05-06 00:36:18 -0400

    feat(front): style buttons

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -12,16 +12,19 @@ const Home = () => {
         <div className='flex gap-10'>
           <div className='container'>
             <h1>New Moment</h1>
-            <p>Create a new moment to work on</p>
+            <p>Create a new Moment to work on</p>
+            <button className='mt-4'>New Moment</button>
           </div>
           <div className='container'>
             <h1>Return to session</h1>
-            <p>Return to where you last left off, at your last edited note</p>
+            <p>Return to where you last left off, at your last edited Moment</p>
+            <button className='mt-4'>Return to Moment</button>
           </div>
         </div>
         <div className='container w-full mt-10'>
             <h1>Study "insert name of flashcards"</h1>
             <p>Study your ai-generated flashcards</p>
+            <button className='mt-4'>Study</button>
         </div>
         <div className='container w-full mt-10'>
           <h1>Your Moments</h1>
