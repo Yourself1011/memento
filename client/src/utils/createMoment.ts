d416318a8f0756f463c511a63193785aebfe61d7
Daniel Zhang <d.zhang200788@gmail.com>
2023-05-06 16:18:19 -0400

    feet: create moment from ocr text among among

--- client/src/utils/createMoment.ts
+++ client/src/utils/createMoment.ts
@@ -1,26 +1,26 @@
 import { Moment } from "../types/moment";
 
-export function createMoment() {
-    const moments = JSON.parse(localStorage.getItem("moments") as string);
+export function createMoment(text = "") {
+  const moments = JSON.parse(localStorage.getItem("moments") as string);
 
-    if (!moments) {
-        localStorage.setItem(
-            "moments",
-            JSON.stringify([
-                {
-                    name: `Untitled Moment 0`,
-                    text: "",
-                    createdDate: new Date().toJSON().slice(0,10).replace(/-/g,'/')
-                },
-            ] as Moment[])
-        );
-    } else {
-        moments.push({
-            name: `Untitled Moment ${moments.length}`,
-            text: "",
-            createdDate: new Date().toJSON().slice(0,10).replace(/-/g,'/')
-        });
-        localStorage.setItem("moments", JSON.stringify(moments));
-    }
-    return moments.length - 1;
+  if (!moments) {
+    localStorage.setItem(
+      "moments",
+      JSON.stringify([
+        {
+          name: `Untitled Moment 0`,
+          text: text,
+          createdDate: new Date().toJSON().slice(0, 10).replace(/-/g, "/"),
+        },
+      ] as Moment[])
+    );
+  } else {
+    moments.push({
+      name: `Untitled Moment ${moments.length}`,
+      text: text,
+      createdDate: new Date().toJSON().slice(0, 10).replace(/-/g, "/"),
+    });
+    localStorage.setItem("moments", JSON.stringify(moments));
+  }
+  return moments.length - 1;
 }
