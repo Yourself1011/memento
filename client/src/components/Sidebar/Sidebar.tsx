dd0f0190a165193a2645bf16955a0c90728f763f
Jeffrey Zang <66485719+jeffrey-zang@users.noreply.github.com>
2023-05-06 03:14:50 +0000

    feat(front): static 'create' button

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -5,20 +5,26 @@ import './Sidebar.scss'
 
 function Sidebar() {
   return ( 
-    <div className="border-r-[#e7e7ef] p-6 border-r-2 flex flex-col sidebar">
-      <p className='font-bold text-2xl text-center'>Amonus</p>
-      <NavLink to='/'>
-        <GrHomeRounded/>
-        Home
-      </NavLink>
-      <NavLink to='/edit'>
-        <GrDocument/>
-        Edit
-      </NavLink>
-      <NavLink to='/cards'>
-        <BsCardList/>
-        Flashcards
-      </NavLink>
+    <div className="border-r-[#e7e7ef] p-6 border-r-2 flex flex-col sidebar justify-between">
+      <div>
+        <p className='font-bold text-2xl text-center'>Amonus</p>
+        <NavLink to='/'>
+          <GrHomeRounded/>
+          Home
+        </NavLink>
+        <NavLink to='/edit'>
+          <GrDocument/>
+          Edit
+        </NavLink>
+        <NavLink to='/cards'>
+          <BsCardList/>
+          Flashcards
+        </NavLink>
+      </div>
+      <div>
+        <a className='create'>+ Create</a>
+      </div>
+
     </div> 
   );
 }
