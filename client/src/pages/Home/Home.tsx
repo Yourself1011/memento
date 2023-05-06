5161d0607661f9653448be84e4680c20f0dccdaa
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-05-06 13:42:13 -0400

    fix(front): make home page buttons do stuff

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -2,8 +2,24 @@ import cohere from "../../assets/cohere.png";
 import MomentList from "../../components/MomentList/MomentList";
 import { createMoment } from "../../utils/createMoment";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+    const navigate = useNavigate();
+    const [lastEdited, setLastEdited] = useState(
+        localStorage.getItem("lastEdited") ?? "0"
+      );
+    
+      function storageChange() {
+        setLastEdited(localStorage.getItem("lastEdited") ?? lastEdited);
+      }
+    
+      useEffect(() => {
+        storageChange();
+        window.addEventListener("storage", storageChange);
+        return window.removeEventListener("storage", storageChange);
+      }, [navigate]);
+    
     return (
         <div className="w-full home">
             <div className="w-full bg-accentbutyoucanbarelyseeit p-6">
@@ -23,7 +39,11 @@ const Home = () => {
                     <div className="container">
                         <h1>New Moment</h1>
                         <p>Create a new Moment to work on</p>
-                        <button className="mt-4">New Moment</button>
+                        <button className="mt-4" onClick={() => {
+                            const momentId = createMoment();
+                            navigate(`/edit/${momentId}`);
+                            location.reload();
+                        }}>New Moment</button>
                     </div>
                     <div className="container">
                         <h1>Return to session</h1>
@@ -31,13 +51,13 @@ const Home = () => {
                             Return to where you last left off, at your last
                             edited Moment
                         </p>
-                        <button className="mt-4">Return to Moment</button>
+                        <button onClick={() => navigate(`/edit/${lastEdited}`)} className="mt-4">Return to Moment</button>
                     </div>
                 </div>
                 <div className="container w-full mt-10">
                     <h1>Study "insert name of flashcards"</h1>
                     <p>Study your ai-generated flashcards</p>
-                    <button className="mt-4">Study</button>
+                    <button className="mt-4" onClick={() => navigate('/cards')}>Study</button>
                 </div>
                 <div className="container w-full mt-10">
                     <h1>Your Moments</h1>
