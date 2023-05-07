c9395625fa45379b4d8c4131fb9fba9e19c8a0f5
Li Feng Yin <lifeng-yin@proton.me>
2023-05-07 01:19:56 -0400

    feat(back): add signUp service to route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import express, { Express, Request, Response } from 'express'
+import mongoose from 'mongoose'
 import cors from 'cors'
 
 const app: Express = express()
@@ -12,6 +13,18 @@ app.get('/', (req: Request, res: Response) => {
     res.json({ 'message': 'Hello World and welcome to the API for Memento!'})
 })
 
-app.listen(5000, () => {
-  console.log(`🚀 Server is running on port: 5000`);
+const port = process.env.SERVER_PORT || 5000
+
+console.log("Starting Express server...")
+
+app.listen(5000, async () => {
+  console.log(`🚀 Server is running on port ${port}!`);
+
+  console.log('Connecting to MongoDB...')
+  try {
+    await mongoose.connect(process.env.MONGODB_URI)
+    console.log('🌱 Finished connecting to MongoDB! ')
+  } catch (error) {
+    console.error('Error while connecting to MongoDB,', error)
+  }
 });
