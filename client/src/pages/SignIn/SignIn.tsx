b1ea6aecb93c51d271e4466eeb65a13cef7f6a75
Daniel Zhang <d.zhang200788@gmail.com>
2023-05-07 08:10:56 -0400

    fix: hehehe

--- client/src/pages/SignIn/SignIn.tsx
+++ client/src/pages/SignIn/SignIn.tsx
@@ -2,75 +2,76 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function SignIn() {
-  const navigate = useNavigate()
+    const navigate = useNavigate();
 
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const [fetchedResult, setFetchedResult] = useState<any>(null)
+    const [fetchedResult, setFetchedResult] = useState<any>(null);
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    if (/\s/.test(username)) {
-      return alert('Username cannot contain any spaces.')
-    }
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        if (/\s/.test(username)) {
+            return alert("Username cannot contain any spaces.");
+        }
 
-    try {
-      const response = await fetch('http://localhost:5000/auth', {
-        method: 'POST',
-        body: JSON.stringify({ username, email, password })
-      })
+        try {
+            const response = await fetch("http://localhost:5000/auth", {
+                method: "POST",
+                body: JSON.stringify({ username, email, password }),
+            });
 
-      const result = await response.json()
-      setFetchedResult(result.message)
-      navigate('/home')
-      
-    } catch (e: any) {
-      console.log(e)
-      setFetchedResult(e.message)
-    }
-  }
+            const result = await response.json();
+            setFetchedResult(result.message);
+            navigate("/home");
+        } catch (e: any) {
+            console.log(e);
+            setFetchedResult(e.message);
+        }
+    };
 
-  return (
-    <div className="p-16 w-[75svh]">
-      <form onSubmit={handleSubmit}>
-        <h1>Sign In/Sign Up</h1>
-        <label className="block mt-8">
-          Username:
-          <input
-            type="text"
-            className="block border-[#ddd] border rounded-[5px] p-4 w-full"
-            onChange={e => setUsername(e.target.value)}
-            value={username}
-            required
-          />
-        </label>
-        <label className="block mt-8">
-          Email:
-          <input
-            type="email"
-            className="block border-[#ddd] border rounded-[5px] p-4 w-full"
-            onChange={e => setEmail(e.target.value)}
-            value={email}
-            required
-          />
-        </label>
-        <label className="block mt-8">
-          Password:
-          <input
-            type="password"
-            className="block border-[#ddd] border rounded-[5px] p-4 w-full"
-            onChange={e => setPassword(e.target.value)}
-            value={password}
-            required
-          />
-        </label>
-        <button type="submit" className="mt-8 bg-accent text-white">Submit</button>
-        {fetchedResult && <p>{fetchedResult?.message}</p>}
-    </form>
-  </div> 
-  );
+    return (
+        <div className="p-16 w-[75svh]">
+            <form onSubmit={handleSubmit}>
+                <h1>Sign In/Sign Up</h1>
+                <label className="block mt-8">
+                    Username:
+                    <input
+                        type="text"
+                        className="block border-[#ddd] border rounded-[5px] p-4 w-full"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={username}
+                        required
+                    />
+                </label>
+                <label className="block mt-8">
+                    Email:
+                    <input
+                        type="email"
+                        className="block border-[#ddd] border rounded-[5px] p-4 w-full"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        required
+                    />
+                </label>
+                <label className="block mt-8">
+                    Password:
+                    <input
+                        type="password"
+                        className="block border-[#ddd] border rounded-[5px] p-4 w-full"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        required
+                    />
+                </label>
+                <button type="submit" className="mt-8 bg-accent text-white">
+                    Submit
+                </button>
+                {fetchedResult && <p>{fetchedResult?.message}</p>}
+            </form>
+        </div>
+    );
 }
 
 export default SignIn;
