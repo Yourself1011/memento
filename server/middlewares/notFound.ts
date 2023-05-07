3cabecba995be1f7d7b95cf2a9f2ac6c85b3a687
Li Feng Yin <lifeng-yin@proton.me>
2023-05-07 01:55:32 -0400

    fix(back): remove duplicated slash in 404 endpoint

--- server/middlewares/notFound.ts
+++ server/middlewares/notFound.ts
@@ -2,7 +2,7 @@ import express, { Request, Response, NextFunction } from 'express'
 
 const notFound = (req: Request, res: Response, next: NextFunction) => {
     res.status(404).json({
-        message: `Could not find route with endpoint /${req.path} and method ${req.method}.`
+        message: `Could not find route with endpoint ${req.path} and method ${req.method}.`
     })
 }
 
