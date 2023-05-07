1a87941d5d39ac60f50603ceb74e21d02dea690e
Li Feng Yin <lifeng-yin@proton.me>
2023-05-07 01:20:39 -0400

    feat(back): add service logic to login route

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,5 +1,21 @@
-import express, { Router } from 'express'
+import express, { Router, Request, Response, NextFunction } from 'express'
+import { signUp } from '../services/auth.ts'
 
 const router: Router = express.Router()
+
+router.post('/login', (req: Request, res: Response, next: NextFunction) => {
+    const { username, email, password } = req.body;
+
+    signUp({ username, email, password })
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(error => {
+            res.status(400).json({
+                message: error
+            })
+            next(error)
+        })
+})
 
 export default router
